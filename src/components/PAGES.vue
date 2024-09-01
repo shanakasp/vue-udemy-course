5b58c2970ef75fdf050353a43ea6cf3282405615
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-01 15:28:45 +0530

    modified PAGES.vue in components

--- src/components/PAGES.vue
+++ src/components/PAGES.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    <h1 class="h1">Hello SHanaka</h1>
+    <h1 class="h1">Hello Shanaka {{ email }}</h1>
+    <h2>{{ number }}</h2>
+    <h3>{{ getName(111) }}</h3>
+    <h3>User Data: {{ getData() }}</h3>
+    <h3>{{ getData().email }}</h3>
+    <h3>{{ getData().phoneNumber }}</h3>
   </div>
 </template>
 
 <script>
 export default {
   name: "PAGES",
+
+  data() {
+    return {
+      email: "shanakaprince@gmail.com",
+      number: "22222",
+    };
+  },
+
+  methods: {
+    getName(a) {
+      return a;
+    },
+    getData() {
+      return {
+        email: "abc@gmail.com",
+        phoneNumber: 123456,
+      };
+    },
+  },
 };
 </script>
 
