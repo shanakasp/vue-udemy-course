11ab17550b26a7f1e9c477b7e55b982e3aacbccb
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-05 21:00:53 +0530

    modified HomeDynamicComponent.vue added AlertNotification.vue

---
+++ src/components/AlertNotification.vue
@@ -0,0 +1,47 @@
+<template>
+  <teleport to="body">
+    <div v-if="isVisible" class="notification-alert">
+      {{ message }}
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: "AlertNotification",
+  props: {
+    message: {
+      type: String,
+      default: "Notification message!",
+    },
+  },
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+  methods: {
+    show() {
+      this.isVisible = true;
+      setTimeout(() => {
+        this.isVisible = false;
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.notification-alert {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  background-color: #4caf50;
+  color: white;
+  padding: 15px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+  transition: opacity 0.5s;
+}
+</style>
--- src/components/HomeDynamicComponent.vue
+++ src/components/HomeDynamicComponent.vue
@@ -6,26 +6,42 @@
     <button @click="currentComponent = 'ComponentA'">Show Component A</button>
     <button @click="currentComponent = 'ComponentB'">Show Component B</button>
 
+    <!-- Button to trigger notification -->
+    <button @click="triggerNotification">Click to Notify</button>
+
     <!-- Conditional rendering of components -->
     <ComponentA v-if="currentComponent === 'ComponentA'" />
     <ComponentB v-if="currentComponent === 'ComponentB'" />
+
+    <!-- AlertNotification Component -->
+    <AlertNotification
+      ref="alertNotification"
+      message="Action Completed Successfully!"
+    />
   </div>
 </template>
 
 <script>
 import ComponentA from "./ComponentA.vue";
 import ComponentB from "./ComponentB.vue";
+import AlertNotification from "./AlertNotification.vue"; // Ensure this is the correct path and name
 
 export default {
   name: "HomeDynamicComponent",
   components: {
     ComponentA,
     ComponentB,
+    AlertNotification,
   },
   data() {
     return {
       currentComponent: "ComponentA",
     };
+  },
+  methods: {
+    triggerNotification() {
+      this.$refs.alertNotification.show();
+    },
   },
 };
 </script>
